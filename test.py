4402f9d80299249614a44b6bba4b446fbe7a4e79
Parth Shukla <parth@pop-os.localdomain>
2020-12-15 12:47:22 +0530

    changes

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-import pickle
+import json
+import os
+
+anno = json.load(open(os.path.join('dataset/val', "via_region_data.json")))
+anno = anno['_via_img_metadata']
+anno = json.dumps(anno)
+print(type(anno))
+print(anno)
+
 
